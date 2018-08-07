9e169411dbc51f7edabf7ffe88f0bb1c24d32e3b
xiaoshuang.li <qq1157502759@gmail.com>
2018-08-07 13:59:44 +0800

    test

--- just-for-fun/ssl/.web.sh
+++ just-for-fun/ssl/.web.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
-mkdir /root/app/challenges
+mv /root/jff/nginx/nginx.conf /etc/nginx/
+mv /root/jff/nginx/base /etc/nginx/sites-enabled/
 
-openssl genrsa 4096 > /root/app/acme-tiny/account.key
-openssl genrsa 4096 > /root/app/acme-tiny/domain.key
-openssl req -new -sha256 -key /root/app/acme-tiny/domain.key -subj "/" -reqexts SAN -config <(cat /etc/ssl/openssl.cnf <(printf "[SAN]\nsubjectAltName=DNS:xiaodifang.club,DNS:www.xiaodifang.club,DNS:xiaoxiaodifang.com,DNS:www.xiaoxiaodifang.com")) > /root/app/acme-tiny/domain.csr
+mkdir /root/jff/challenges
 
-touch /root/app/acme-tiny/chained.pem
-mv /root/app/nginx/nginx.conf /etc/nginx/
-mv /root/app/nginx/jff /etc/nginx/sites-enabled/
-nginx
-rm /root/app/acme-tiny/chained.pem
+openssl genrsa 4096 > /root/jff/account.key
+openssl genrsa 4096 > /root/jff/domain.key
+openssl req -new -sha256 -key /root/jff/domain.key -subj "/" -reqexts SAN -config <(cat /etc/ssl/openssl.cnf <(printf "[SAN]\nsubjectAltName=DNS:xiaodifang.club,DNS:www.xiaodifang.club,DNS:xiaoxiaodifang.com,DNS:www.xiaoxiaodifang.com")) > /root/jff/domain.csr
 
-python /root/app/acme-tiny/acme_tiny.py --account-key /root/app/acme-tiny/account.key --csr /root/app/acme-tiny/domain.csr --acme-dir /root/app/challenges/ > /tmp/signed.crt || exit
+touch /root/jff/chained.pem
+rm /root/jff/chained.pem
+
+python /root/jff/acme_tiny.py --account-key /root/jff/account.key --csr /root/jff/domain.csr --acme-dir /root/jff/challenges/ > /tmp/signed.crt || exit
 wget -O - https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem > intermediate.pem
-cat /tmp/signed.crt intermediate.pem > /root/app/acme-tiny/chained.pem
+cat /tmp/signed.crt intermediate.pem > /root/jff/chained.pem
 
+mv /root/jff/nginx/jff /etc/nginx/sites-enabled/
 nginx
 nginx -s reload
 service nginx restart
+cd ../
+npm run start
