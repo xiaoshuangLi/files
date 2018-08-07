b315dcdfc3079ddd0401b56d950c8da31f03a990
xiaoshuang.li <qq1157502759@gmail.com>
2018-08-07 18:58:53 +0800

    complete 5.0

--- just-for-fun/ssl/.all.sh
+++ just-for-fun/ssl/.all.sh
@@ -7,12 +7,14 @@ apt-get install -y git nginx curl bash python openssl docker docker.io
 docker pull xiaoshuang/jff
 docker run -p 8111:8111 xiaoshuang/jff
 
-mkdir /var/log
-touch /var/log/acme_tiny.log
+touch BASE/acme_tiny.log
 
 cp $BASE/nginx/nginx.conf /etc/nginx/
 cp $BASE/nginx/base /etc/nginx/sites-enabled/
+
+openssl dhparam -dsaparam -out /etc/ssl/certs/dhparam.pem 4096
 nginx
+
 rm /etc/nginx/sites-enabled/base
 
 git clone https://github.com/diafygi/acme-tiny.git
@@ -24,4 +26,4 @@ mkdir /just-for-fun/jff/challenges
 
 bash $BASE/ssl/.web.sh
 
-0 0 1 * * $BASE/ssl/.web.sh 2>> /var/log/acme_tiny.log
+0 0 1 * * $BASE/ssl/.web.sh 2>> BASE/acme_tiny.log
